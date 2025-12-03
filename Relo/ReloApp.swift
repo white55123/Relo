cdfe47b221bc26baf9a2060288330ac4613f31ee
reollzhao <763142485@qq.com>
2025-12-03 20:44:23 +0800

    Initial Commit

---
+++ Relo/ReloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReloApp.swift
+//  Relo
+//
+//  Created by reol on 2025/12/3.
+//
+
+import SwiftUI
+
+@main
+struct ReloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
